6ee73779d8ae4c9bb07c4dcdc0a2ebcce5fa6f7e
XDMUSIX <147618918+XDMUSIX@users.noreply.github.com>
2024-02-24 10:27:51 +0530

    Add files via upload
    Signed-off-by: XDMUSIX <147618918+XDMUSIX@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/f6cabf06615d4bbdc4136.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/781e980163d5a78afeeae.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/About_Info_Devil")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Devilxdiscuss")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://telegra.ph/file/a72e3a22e9bc43d35cec9.jpg"
